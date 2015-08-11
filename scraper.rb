05afe7aa3968422f9e8a350c6495895f2dbb90d1
Tony Bowden <tony@tmtm.com>
2015-08-11 11:45:03 +0100

    Default to English
    We now have English versions of name, area, and party, so use those by
    default, and store the others as __ne

--- scraper.rb
+++ scraper.rb
@@ -7,30 +7,30 @@ require 'json'
 require 'nokogiri'
 require 'scraperwiki'
 
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
+# require 'open-uri/cached'
+# OpenURI::Cache.cache_path = '.cache'
 
 def reprocess_csv(file)
   raw = open(file).read.force_encoding("UTF-8")
   csv = CSV.parse(raw.lines.drop(2).join)
-  csv.each do |row|
+  csv.each_with_index do |row, i|
     next if row[0].to_s.empty?
     next if row[8].to_s.empty?
     next if row[0].to_s.include? 'Development Region'
     data = { 
-      name: row[2].to_s.strip,
-      name__en: row[8].to_s.strip,
-      area: row[1].to_s.strip,
-      area__en: row[7].to_s.strip,
+      name: row[8].to_s.strip,
+      name__ne: row[2].to_s.strip,
+      area: row[7].to_s.strip,
+      area__ne: row[1].to_s.strip,
+      party: row[11].to_s.strip,
+      party__ne: row[5].to_s.strip,
       email: row[6].to_s.strip,
-      party: row[5].to_s.strip,
       mobile: (row[3] || "").lines.first.to_s.strip,
       phone: (row[4] || "").lines.first.to_s.strip,
       term: 'ca2',
     }
-    ScraperWiki.save_sqlite([:name, :term], data)
+    ScraperWiki.save_sqlite([:name, :area, :term], data)
   end
 end
 
 csv_data = reprocess_csv('https://docs.google.com/spreadsheets/d/1yRtq9B81vgPJ373nwhjuwwdDp-FnlyI17QMOAQRrcVE/export?format=csv&gid=18')
-
