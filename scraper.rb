af2691d6320932a55d0b12b4bfeb8918a666e793
octopusinvitro <octopusinvitro@users.noreply.github.com>
2016-07-15 17:30:04 +0100

    Aligh fields to aid with readability

--- scraper.rb
+++ scraper.rb
@@ -18,16 +18,16 @@ def reprocess_csv(file)
     next if row[9].to_s.empty?
     next if row[0].to_s.include? 'Development Region'
     data = {
-      name: row[9].to_s.strip,
-      name__ne: row[2].to_s.strip,
-      area: row[8].to_s.strip,
-      area__ne: row[1].to_s.strip,
-      party: row[12].to_s.strip,
+      name:      row[9].to_s.strip,
+      name__ne:  row[2].to_s.strip,
+      area:      row[8].to_s.strip,
+      area__ne:  row[1].to_s.strip,
+      party:     row[12].to_s.strip,
       party__ne: row[6].to_s.strip,
-      email: row[7].to_s.strip,
-      mobile: (row[3] || "").lines.first.to_s.strip,
-      phone:  (row[5] || "").lines.first.to_s.strip,
-      term: 'ca2',
+      email:     row[7].to_s.strip,
+      mobile:    (row[3] || "").lines.first.to_s.strip,
+      phone:     (row[5] || "").lines.first.to_s.strip,
+      term:      'ca2',
     }
     ScraperWiki.save_sqlite([:name, :area, :term], data)
   end
